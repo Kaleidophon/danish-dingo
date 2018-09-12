Adding corrected accuracy computation and buggy training.

assignment_1/code/mlp_numpy.py
@@ -10,73 +10,73 @@ import modules
 
 
 class MLP(object):
-  """
-  This class implements a Multi-layer Perceptron in NumPy.
-  It handles the different layers and parameters of the model.
-  Once initialized an MLP object can perform forward and backward.
-  """
-
-  def __init__(self, n_inputs, n_hidden, n_classes):
     """
-    Initializes MLP object. 
-    
-    Args:
-      n_inputs: number of inputs.
-      n_hidden: list of ints, specifies the number of units
-                in each linear layer. If the list is empty, the MLP
-                will not have any linear layers, and the model
-                will simply perform a multinomial logistic regression.
-      n_classes: number of classes of the classification problem.
-                 This number is required in order to specify the
-                 output dimensions of the MLP
+    This class implements a Multi-layer Perceptron in NumPy.
+    It handles the different layers and parameters of the model.
+    Once initialized an MLP object can perform forward and backward.
     """
-    self.input_layer = modules.LinearModule(n_inputs, n_classes if len(n_hidden) == 0 else n_hidden[0])
-    self.hidden_layers = [modules.ReLUModule()]
 
-    # TODO: Make this more pretty
-    layer_sizes = n_hidden + [n_classes]
-    for layer, next_hidden in list(enumerate(layer_sizes))[1:]:
-        self.hidden_layers.append(modules.LinearModule(layer_sizes[layer - 1], next_hidden))
+    def __init__(self, n_inputs, n_hidden, n_classes):
+        """
+        Initializes MLP object.
 
-        if layer != len(layer_sizes) - 1:
-          self.hidden_layers.append(modules.ReLUModule())
+        Args:
+          n_inputs: number of inputs.
+          n_hidden: list of ints, specifies the number of units
+                    in each linear layer. If the list is empty, the MLP
+                    will not have any linear layers, and the model
+                    will simply perform a multinomial logistic regression.
+          n_classes: number of classes of the classification problem.
+                     This number is required in order to specify the
+                     output dimensions of the MLP
+        """
+        self.input_layer = modules.LinearModule(n_inputs, n_classes if len(n_hidden) == 0 else n_hidden[0])
+        self.hidden_layers = [modules.ReLUModule()]
+        self.learned_modules = [self.input_layer]
 
-    self.output_layer = modules.SoftMaxModule()
+        # TODO: Make this more pretty
+        layer_sizes = n_hidden + [n_classes]
+        for layer, next_hidden in list(enumerate(layer_sizes))[1:]:
+            linear = modules.LinearModule(layer_sizes[layer - 1], next_hidden)
+            self.hidden_layers.append(linear)
+            self.learned_modules.append(linear)
 
-  def forward(self, x):
-    """
-    Performs forward pass of the input. Here an input tensor x is transformed through 
-    several layer transformations.
-    
-    Args:
-      x: input to the network
-    Returns:
-      out: outputs of the network
-    """
-    out = self.input_layer.forward(x)
+            if layer != len(layer_sizes) - 1:
+                self.hidden_layers.append(modules.ReLUModule())
 
-    for hidden_layer in self.hidden_layers:
-        out = hidden_layer.forward(out)
+        self.output_layer = modules.SoftMaxModule()
 
-    out = self.output_layer.forward(out)
+    def forward(self, x):
+        """
+        Performs forward pass of the input. Here an input tensor x is transformed through
+        several layer transformations.
 
-    return out
+        Args:
+          x: input to the network
+        Returns:
+          out: outputs of the network
+        """
+        out = self.input_layer.forward(x)
 
-  def backward(self, dout):
-    """
-    Performs backward pass given the gradients of the loss. 
+        for hidden_layer in self.hidden_layers:
+            out = hidden_layer.forward(out)
 
-    Args:
-      dout: gradients of the loss
-    """
-    dout = self.output_layer.backward(dout)
+        out = self.output_layer.forward(out)
 
-    for hidden_layer in self.hidden_layers[::-1]:
-        dout = hidden_layer.backward(dout)
+        return out
 
-    dout = self.input_layer.backward(dout)
+    def backward(self, dout):
+        """
+        Performs backward pass given the gradients of the loss.
 
-    return dout
+        Args:
+          dout: gradients of the loss
+        """
+        dout = self.output_layer.backward(dout)
 
-  import torch
-  torch.nn.Linear
+        for hidden_layer in self.hidden_layers[::-1]:
+            dout = hidden_layer.backward(dout)
+
+        dout = self.input_layer.backward(dout)
+
+        return dout

assignment_1/code/modules.py
@@ -50,10 +50,14 @@ class LinearModule(object):
           dx: gradients with respect to the input of the module
         """
         self.grads["weight"] = self.x.T @ dout
-        self.grads["bias"] = dout
+        self.grads["bias"] = dout.mean(axis=0)
         dx = dout @ self.params["weight"].T
 
         return dx
+
+    def update_parameters(self, learning_rate):
+        self.params["weight"] -= learning_rate * self.grads["weight"]
+        self.params["bias"] -= learning_rate * self.grads["bias"]
 
 
 class ReLUModule(object):
@@ -111,16 +115,16 @@ class SoftMaxModule(object):
           out: output of the module                                                        #
         """
         self.x = x
-        self.dim = x.shape[0]
         out = self.softmax(x)
 
         return out
 
     @staticmethod
     def softmax(x):
-        z = x.max(axis=1)[..., np.newaxis]
+        z = x.max(axis=1, keepdims=True)
         z = np.repeat(z, x.shape[1], axis=1)
-        out = np.exp(x - z) / np.exp(x - z).sum(axis=1)[..., np.newaxis]
+        out = np.exp(x - z) / np.exp(x - z).sum(axis=1, keepdims=True)
+        test = out.sum(axis=1)  # TODO: Remove, just test if softmax adds up to 1
         return out
 
     def backward(self, dout):
@@ -164,8 +168,10 @@ class CrossEntropyModule(object):
         Returns:
           out: cross entropy loss
         """
+        epsilon = 1e-10
+        x[x == 0] = epsilon
         out = - (np.log(x) * y).sum(axis=1)
-        out = out.mean()  # Divide by unit test
+        out = out.mean()  # Divide by batch size
 
         return out
 

assignment_1/code/train_mlp_numpy.py
@@ -27,6 +27,7 @@ DATA_DIR_DEFAULT = './cifar10/cifar-10-batches-py'
 N_INPUTS = 3072
 N_CLASSES = 10
 EPOCHS_DEFAULT = 5
+LR_DEFAULT = 0.01
 
 FLAGS = None
 
@@ -45,12 +46,14 @@ def accuracy(predictions, targets):
       accuracy: scalar float, the accuracy of predictions,
                 i.e. the average correct predictions over the whole batch
     """
-    acc = (predictions == targets).sum()
+    acc = (predictions.argmax(axis=1) == targets.argmax(axis=1))
+    acc = acc.astype(int)
+    acc = acc.mean()
 
     return acc
 
 
-def train(epochs=EPOCHS_DEFAULT, batch_size=BATCH_SIZE_DEFAULT):
+def train(epochs=EPOCHS_DEFAULT, batch_size=BATCH_SIZE_DEFAULT, learning_rate=LR_DEFAULT):
     """
     Performs training and evaluation of MLP model.
 
@@ -77,18 +80,38 @@ def train(epochs=EPOCHS_DEFAULT, batch_size=BATCH_SIZE_DEFAULT):
     # Initialize model
     nn = MLP(n_inputs=N_INPUTS, n_hidden=dnn_hidden_units, n_classes=N_CLASSES)
     loss_func = CrossEntropyModule()
+    completed_epochs = 0
+    batch_nr = 1
 
-    while train_set.epochs_completed < epochs:
+    while completed_epochs < epochs:
         x, y = train_set.next_batch(BATCH_SIZE_DEFAULT)
+
+        if train_set.epochs_completed > completed_epochs:
+            batch_nr = 1
+
         x = x.reshape(batch_size, 32 * 32 * 3)
 
         # Forward pass and loss
         out = nn.forward(x)
         loss = loss_func.forward(out, y)
+        acc = accuracy(out, y)
+        print("[Epoch {}/{} | Batch #{}] Loss: {:.2f} | Accuracy: {:.2f}".format(
+                completed_epochs + 1, epochs, batch_nr, loss, acc
+            )
+        )
 
         # Backward pass
         loss_gradient = loss_func.backward(out, y)
         nn.backward(loss_gradient)
+
+        # Adjust parameters
+        for module in nn.learned_modules:
+            module.update_parameters(learning_rate)
+            print("Weight norm: {:.4f}".format(np.linalg.norm(module.params["weight"])))
+
+        # Prepare for next iteration
+        completed_epochs = train_set.epochs_completed
+        batch_nr += 1
 
 
 def print_flags():