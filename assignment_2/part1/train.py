93f8e7bab3b1ead2d4337e15d797e71a0cf9379c
Kaleidophon <Dennis.Ulmer@gmx.de>
2018-09-20 15:39:21 +0200

    First version of vanilla RNN implementation, but the accuracy is low.

--- assignment_2/part1/train.py
+++ assignment_2/part1/train.py
@@ -25,6 +25,7 @@ import numpy as np
 
 import torch
 from torch.utils.data import DataLoader
+from torch.autograd import Variable
 
 from part1.dataset import PalindromeDataset
 from part1.vanilla_rnn import VanillaRNN
@@ -35,6 +36,26 @@ from part1.lstm import LSTM
 
 ################################################################################
 
+
+def calculate_accuracy(predictions, targets):
+    """
+    Computes the prediction accuracy, i.e. the average of correct predictions
+    of the network.
+
+    Args:
+      predictions: 2D float array of size [batch_size, n_classes]
+      labels: 2D int array of size [batch_size, n_classes]
+              with one-hot encoding. Ground truth labels for
+              each sample in the batch
+    Returns:
+      accuracy: scalar float, the accuracy of predictions,
+                i.e. the average correct predictions over the whole batch
+    """
+    accurate_predictions = predictions.argmax(dim=1) == targets
+    acc = float(accurate_predictions.cpu().float().mean(dim=0).numpy())
+    return acc
+
+
 def train(config):
 
     assert config.model_type in ('RNN', 'LSTM')
@@ -43,33 +64,42 @@ def train(config):
     device = torch.device(config.device)
 
     # Initialize the model that we are going to use
-    model = None  # fixme
+    model_kwargs = {
+        "seq_length": config.input_length, "input_dim": config.input_dim, "num_hidden": config.num_hidden,
+        "num_classes": config.num_classes, "batch_size": config.batch_size, "device": config.device
+    }
+    model = VanillaRNN(**model_kwargs) if config.model_type == "RNN" else LSTM(**model_kwargs)
+    input_length = config.input_length
+    batch_size = config.batch_size
 
     # Initialize the dataset and data loader (note the +1)
     dataset = PalindromeDataset(config.input_length+1)
     data_loader = DataLoader(dataset, config.batch_size, num_workers=1)
 
     # Setup the loss and optimizer
-    criterion = None  # fixme
-    optimizer = None  # fixme
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=config.learning_rate)
 
     for step, (batch_inputs, batch_targets) in enumerate(data_loader):
 
         # Only for time measurement of step through network
         t1 = time.time()
+        optimizer.zero_grad()
 
-        # Add more code here ...
+        for x_t in batch_inputs.split(1, dim=1):
+            x_t = Variable(x_t)
+            out = model(x_t)
 
-        ############################################################################
-        # QUESTION: what happens here and why?
-        ############################################################################
+        # Calculate loss and stuff
+        y_t = Variable(batch_targets)
+
+        loss = criterion(out, y_t)  # Only use last output for prediction
+        accuracy = calculate_accuracy(out, y_t)
+
+        # Clip gradients in order to avoid the exploding gradient problem during backward step
+        loss.backward()
         torch.nn.utils.clip_grad_norm(model.parameters(), max_norm=config.max_norm)
-        ############################################################################
-
-        # Add more code here ...
-
-        loss = np.inf   # fixme
-        accuracy = 0.0  # fixme
+        optimizer.step()
 
         # Just for time measurement
         t2 = time.time()
@@ -77,12 +107,13 @@ def train(config):
 
         if step % 10 == 0:
 
-            print("[{}] Train Step {:04d}/{:04d}, Batch Size = {}, Examples/Sec = {:.2f}, "
-                  "Accuracy = {:.2f}, Loss = {:.3f}".format(
+            print(
+                "\r[{}] Train Step {:04d}/{:04d}, Examples/Sec = {:.2f}, Accuracy = {:.2f}, Loss = {:.3f}".format(
                     datetime.now().strftime("%Y-%m-%d %H:%M"), step,
                     config.train_steps, config.batch_size, examples_per_second,
                     accuracy, loss
-            ))
+                ), flush=True, end="\n" if step % 1000 else ""
+            )
 
         if step == config.train_steps:
             # If you receive a PyTorch data-loader error, check this bug report:
--- assignment_2/part1/vanilla_rnn.py
+++ assignment_2/part1/vanilla_rnn.py
@@ -20,19 +20,45 @@ from __future__ import print_function
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 ################################################################################
+
 
 class VanillaRNN(nn.Module):
 
     def __init__(self, seq_length, input_dim, num_hidden, num_classes, batch_size, device='cpu'):
         super(VanillaRNN, self).__init__()
+        self.seq_length = seq_length
+        self.num_classes = num_classes
+        self.batch_size = batch_size
+        self.num_hidden = num_hidden
+        self.input_dim = input_dim
 
-        self.W_hx = nn.Parameter(torch.Tensor(input_dim, num_hidden))
-        self.W_hh = nn.Parameter(torch.Tensor(num_hidden, num_hidden))
-        self.b_h = nn.Parameter(torch.Tensor(num_hidden, 1))
+        # Define parameters
+        self.W_hx = nn.Parameter(torch.rand(input_dim, num_hidden)).to(device)
+        self.W_hh = nn.Parameter(torch.rand(num_hidden, num_hidden)).to(device)
+        self.b_h = nn.Parameter(torch.zeros(num_hidden)).to(device)
+        self.W_ph = nn.Parameter(torch.rand(num_hidden, self.num_classes)).to(device)
+        self.b_p = nn.Parameter(torch.zeros(num_classes)).to(device)
 
+        # Define rest
+        self.h_previous = torch.zeros(batch_size, num_hidden)
+        self.t = 0
 
     def forward(self, x):
-        # Implementation here ...
-        pass
+        h_t = F.tanh(x @ self.W_hx + self.h_previous @ self.W_hh + self.b_h)
+        p_t = h_t @ self.W_ph + self.b_p
+
+        # Prepare for next iteration
+        self.t += 1
+
+        # Store hidden state for next input symbol
+        if self.t < self.seq_length:
+            self.h_previous = h_t
+        # Reset to initial hidden state if end of sequence was reached
+        else:
+            self.h_previous = torch.zeros(self.batch_size, self.num_hidden)
+            self.t = 0
+
+        return p_t
