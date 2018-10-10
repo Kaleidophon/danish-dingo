e73da022e41d9307beab8faaaec5076767135b2c
Kaleidophon <Dennis.Ulmer@gmx.de>
2018-10-10 17:11:01 +0200

    Implementing basic gan (CPU).

--- assignment_3/code/a3_gan_template.py
+++ assignment_3/code/a3_gan_template.py
@@ -6,63 +6,96 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 from torchvision import datasets
+from torch.autograd import Variable
+
+# Determine if GPU is available
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 
 class Generator(nn.Module):
-    def __init__(self):
+    def __init__(self, latent_dim, negative_slope=0.2):
         super(Generator, self).__init__()
+        self.latent_dim = latent_dim
 
-        # Construct generator. You are free to experiment with your model,
-        # but the following is a good start:
-        #   Linear args.latent_dim -> 128
-        #   LeakyReLU(0.2)
-        #   Linear 128 -> 256
-        #   Bnorm
-        #   LeakyReLU(0.2)
-        #   Linear 256 -> 512
-        #   Bnorm
-        #   LeakyReLU(0.2)
-        #   Linear 512 -> 1024
-        #   Bnorm
-        #   LeakyReLU(0.2)
-        #   Linear 1024 -> 768
-        #   Output non-linearity
+        self.model = nn.Sequential(
+            nn.Linear(latent_dim, 128),
+            nn.LeakyReLU(negative_slope),
+            nn.Linear(128, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(negative_slope),
+            nn.Linear(256, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(negative_slope),
+            nn.Linear(512, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(negative_slope),
+            nn.Linear(1024, 784),
+            nn.Sigmoid()
+        )
 
     def forward(self, z):
-        # Generate images from z
-        pass
+        return self.model(z)
 
 
 class Discriminator(nn.Module):
-    def __init__(self):
+    def __init__(self, negative_slope=0.2):
         super(Discriminator, self).__init__()
 
-        # Construct distriminator. You are free to experiment with your model,
-        # but the following is a good start:
-        #   Linear 784 -> 512
-        #   LeakyReLU(0.2)
-        #   Linear 512 -> 256
-        #   LeakyReLU(0.2)
-        #   Linear 256 -> 1
-        #   Output non-linearity
+        self.model = nn.Sequential(
+            nn.Linear(784, 512),
+            nn.LeakyReLU(negative_slope),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(negative_slope),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
 
     def forward(self, img):
         # return discriminator score for img
-        pass
+        return self.model(img)
 
 
 def train(dataloader, discriminator, generator, optimizer_G, optimizer_D):
-    for epoch in range(args.n_epochs):
-        for i, (imgs, _) in enumerate(dataloader):
+    criterion = nn.BCELoss()
+    num_batches = len(dataloader)
 
-            imgs.cuda()
+    for epoch in range(args.n_epochs):
+        for i, (images, _) in enumerate(dataloader):
+
+            batch_size = images.size()[0]
+            images = Variable(images.view(batch_size, 28 * 28))
+            images.to(device)
+            latent_dim = generator.latent_dim
 
             # Train Generator
             # ---------------
+            optimizer_G.zero_grad()
+            z = Variable(torch.empty(batch_size, latent_dim).uniform_(0, 1).to(device))  # Sample noise
+            fake_imgs = generator(z)
+            real_labels = torch.ones(batch_size, 1)
+            fake_predictions = discriminator(fake_imgs)
+            generator_loss = criterion(fake_predictions, real_labels)
+            generator_loss.backward()
+            optimizer_G.step()
 
             # Train Discriminator
             # -------------------
             optimizer_D.zero_grad()
+            fake_labels = torch.zeros(batch_size, 1)
+            real_predictions = discriminator(images)
+            real_loss = criterion(real_predictions, real_labels)
+            fake_loss = criterion(fake_predictions.detach(), fake_labels)
+            discriminator_loss = 0.5 * (real_loss + fake_loss)
+            discriminator_loss.backward()
+            optimizer_D.step()
+
+            # Screen output
+            if (i + 1) % args.print_interval == 0:
+                print(
+                    "\r[Epoch {:>2}/{:>2}, Batches {:>3}/{:>3}] | Generator loss: {:.4f} | Discriminator loss: {:.4f}".format(
+                        epoch+1, args.n_epochs, i+1, num_batches, generator_loss, discriminator_loss
+                    ), end="\n" if (i + 1) % (args.print_interval * 10) == 0 and i != 0 else "", flush=True
+                )
 
             # Save Images
             # -----------
@@ -77,7 +110,7 @@ def train(dataloader, discriminator, generator, optimizer_G, optimizer_D):
                 pass
 
 
-def main():
+def main(args):
     # Create output image directory
     os.makedirs('images', exist_ok=True)
 
@@ -91,7 +124,7 @@ def main():
         batch_size=args.batch_size, shuffle=True)
 
     # Initialize models and optimizers
-    generator = Generator()
+    generator = Generator(args.latent_dim)
     discriminator = Discriminator()
     optimizer_G = torch.optim.Adam(generator.parameters(), lr=args.lr)
     optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=args.lr)
@@ -116,6 +149,8 @@ if __name__ == "__main__":
                         help='dimensionality of the latent space')
     parser.add_argument('--save_interval', type=int, default=500,
                         help='save every SAVE_INTERVAL iterations')
+    parser.add_argument('--print_interval', type=int, default=100,
+                        help='save every SAVE_INTERVAL iterations')
     args = parser.parse_args()
 
-    main()
+    main(args)
